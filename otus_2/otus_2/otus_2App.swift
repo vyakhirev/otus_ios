dfce6c8d7c8cd098b5ecd333330f0f31d29fca77
vyahirev.mihail <vyahirev.mihail@rwb.ru>
2026-03-10 17:18:14 +0300

    Initial

---
+++ otus_2/otus_2/otus_2App.swift
@@ -0,0 +1,17 @@
+//
+//  otus_2App.swift
+//  otus_2
+//
+//  Created by Vyahirev.Mihail on 10.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct otus_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
